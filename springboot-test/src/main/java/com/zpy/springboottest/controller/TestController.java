cc1236282d91cd37f107795c77907f6149817015
kuangbendewoniu <695518147@qq.com>
2020-06-30 01:11:54 +0800

    jsperreport pdf

--- springboot-test/src/main/java/com/zpy/springboottest/controller/TestController.java
+++ springboot-test/src/main/java/com/zpy/springboottest/controller/TestController.java
@@ -1,31 +1,58 @@
 package com.zpy.springboottest.controller;
 
-import org.apache.commons.io.IOUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
+import net.sf.jasperreports.engine.export.JRPdfExporterParameter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 @RequestMapping(value = "/test")
+@Slf4j
 public class TestController {
 
     @RequestMapping(value = "/pdf-download", method = GET)
-    public ResponseEntity download() throws IOException {
-        File file = new File("/Users/kuangbendewoniu/IdeaProjects/springboot-program/springboot-test/src/main/resources/HelloWorld.pdf");
-        InputStream in = new FileInputStream(file);
-        final HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", "application/pdf");
-        headers.add("Content-Disposition", "attachment; filename=" + file.getName() );
-        return new ResponseEntity<>(IOUtils.toByteArray(in), headers, HttpStatus.OK);
+    public void download(InputStream in) throws IOException {
+        Map parameters = new HashMap();
+        ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+        FileOutputStream outputStream = null;
+        File file = new File("report.pdf");
+        String reportModelFile = "report2.jasper";
+
+        try {
+            JasperPrint jasperPrint = JasperFillManager.fillReport(reportModelFile,
+                    parameters);
+            JRAbstractExporter exporter = new JRPdfExporter();
+            //创建jasperPrint
+            exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint);
+            //生成输出流
+            exporter.setParameter(JRExporterParameter.OUTPUT_STREAM, outPut);
+            //屏蔽copy功能
+            exporter.setParameter(JRPdfExporterParameter.IS_ENCRYPTED, Boolean.TRUE);
+            //加密
+            exporter.setParameter(JRPdfExporterParameter.IS_128_BIT_KEY, Boolean.TRUE);
+            exporter.exportReport();
+            outputStream = new FileOutputStream(file);
+            outputStream.write(outPut.toByteArray());
+        } catch (JRException e) {
+            log.error("{}",e);
+        } catch (Exception e) {
+            log.error("{}",e);
+        } finally {
+            try {
+                outPut.flush();
+                outPut.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
